a4805952d57d487ffb64099899c2b8ba08d51b84
Waldemar Jankowski <w.jankowski@brstrat.com>
2012-10-29 08:34:42 -0400

    merged in brs changes from baseline commit 575a41978aac0111862bc5b7deb0b7e6782f61c4

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -38,4 +38,28 @@ jQuery.cookie = function (key, value, options) {
     options = value || {};
     var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
     return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+
+
+
+
+
+
+
+
+
+
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
